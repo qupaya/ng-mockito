dc4631584162b665cde688d10551c21108746ebb
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2020-10-17 17:38:24 +0200

    chore: add ng-mockito lib

---
+++ libs/ng-mockito/ng-mockito/README.md
@@ -0,0 +1,7 @@
+# ng-mockito-ng-mockito
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test ng-mockito-ng-mockito` to execute the unit tests.
---
+++ libs/ng-mockito/ng-mockito/jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  displayName: 'ng-mockito-ng-mockito',
+  preset: '../../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: {
+        before: [
+          'jest-preset-angular/build/InlineFilesTransformer',
+          'jest-preset-angular/build/StripStylesTransformer',
+        ],
+      },
+    },
+  },
+  coverageDirectory: '../../../coverage/libs/ng-mockito/ng-mockito',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js',
+  ],
+};
