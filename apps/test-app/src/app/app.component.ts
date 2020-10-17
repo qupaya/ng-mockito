c92e55f8c6f7bbed16ffc9b1bef8e520f66fca08
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2020-10-17 17:21:54 +0200

    chore: rename package to qupaya

--- apps/test-app/src/app/app.component.ts
+++ apps/test-app/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'ng-mockito-root',
+  selector: 'qp-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
