e9cebeebfe4a21ef67d0b0f8d6808c32c0018046
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2020-10-18 17:42:16 +0200

    refactor(ng-mockito): extract createTypeAndMock function

--- libs/ng-mockito/ng-mockito/src/lib/mock-provider.ts
+++ libs/ng-mockito/ng-mockito/src/lib/mock-provider.ts
@@ -1,36 +1,13 @@
-import { FactoryProvider, Type } from '@angular/core';
-import { instance, mock } from 'ts-mockito';
-import { getMockedClass, noOp } from './ts-mockito-helpers';
+import { FactoryProvider } from '@angular/core';
+import { instance } from 'ts-mockito';
+import { createTypeAndMock, noOp, TypeOrMock } from './ts-mockito-helpers';
 
 export function mockProvider<T>(
-  provide: Type<T> | T,
+  provide: TypeOrMock<T>,
   setupMock: (m: T) => void = noOp
 ): FactoryProvider {
-  if (typeof provide === 'object') {
-    return createFactoryProviderFromMock<T>(provide, setupMock);
-  } else if (typeof provide === 'function') {
-    return createFactoryProviderFromClass<T>(provide as Type<T>, setupMock);
-  } else {
-    throw new Error(
-      `Given argument had invalid type. Please provide a class or a ts-mockito mock.`
-    );
-  }
-}
+  const { type, mock } = createTypeAndMock(provide);
 
-function createFactoryProviderFromClass<T>(
-  provide: Type<T>,
-  setupMock: (m: T) => void
-) {
-  const mocked = mock<T>(provide);
-  setupMock(mocked);
-  return { provide, useFactory: () => instance(mocked) };
-}
-
-function createFactoryProviderFromMock<T>(
-  provide: T,
-  setupMock: (m: T) => void
-) {
-  const mockedClass = getMockedClass(provide);
-  setupMock(provide);
-  return { provide: mockedClass, useFactory: () => instance(provide) };
+  setupMock(mock);
+  return { provide: type, useFactory: () => instance(mock) };
 }
