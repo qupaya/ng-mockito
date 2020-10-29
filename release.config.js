683ac7fac945846ea49f86256c11d2628bfbcf84
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2020-10-29 11:43:53 +0100

    fix: disable GitHub release until configuration issue is solved

--- release.config.js
+++ release.config.js
@@ -31,7 +31,6 @@ module.exports = {
       '@semantic-release/npm',
       { npmPublish: true, pkgRoot: './dist/libs/ng-mockito/ng-mockito' },
     ],
-    ['@semantic-release/github', { assets: 'dist/libs/ng-mockito/ng-mockito' }],
     [
       '@semantic-release/git',
       {
