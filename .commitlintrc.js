872c526a3823007f1cad10f32598a19ccfd2108a
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2021-01-24 15:14:54 +0100

    style: allow commit message headers up to length 100

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,1 +1,6 @@
-module.exports = { extends: ['@commitlint/config-angular'] };
+module.exports = {
+  extends: ['@commitlint/config-angular'],
+  rules: {
+    'header-max-length': [0, 'always', 100],
+  },
+};
